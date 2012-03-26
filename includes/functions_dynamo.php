4047a9859762839278d4f9d459648347675ba0c4
dellsystem <ilostwaldo@gmail.com>
2012-03-25 23:03:31 -0400

    Move imagecopymerge_alpha to functions_dynamo.php
    Seemed more appropriate. Also changed the formatting to make the code
    adhere to phpBB's style guidelines.

--- includes/functions_dynamo.php
+++ includes/functions_dynamo.php
@@ -52,4 +52,70 @@ function get_avatar_image_path($user_id)
 	global $config, $phpbb_root_path;
 	return $phpbb_root_path . $config['dynamo_avatar_fp'] . '/' . $user_id . '_' . time() . '.png';
 }
+
+/**
+* From the php.net documentation: http://www.php.net/manual/en/function.imagecopymerge.php
+* Main script by aiden dot mail at freemail dot hu
+* Transformed to imagecopymerge_alpha() by rodrigo dot polo at gmail dot com
+*/
+function merge_images($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct)
+{
+	if (!isset($pct))
+	{
+		return false;
+	}
+
+	$pct /= 100;
+	// Get image width and height
+	$w = imagesx($src_im);
+	$h = imagesy($src_im);
+	// Turn alpha blending off
+	imagealphablending($src_im, false);
+
+	// Find the most opaque pixel in the image (the one with the smallest alpha value)
+	$minalpha = 127;
+	for ($x = 0; $x < $w; $x++)
+	{
+		for ($y = 0; $y < $h; $y++)
+		{
+			$alpha = (imagecolorat($src_im, $x, $y) >> 24) & 0xFF;
+			if ($alpha < $minalpha)
+			{
+				$minalpha = $alpha;
+			}
+		}
+	}
+
+	// Loop through image pixels and modify alpha for each
+	for($x = 0; $x < $w; $x++)
+	{
+		for ($y = 0; $y < $h; $y++)
+		{
+			// Get current alpha value (represents the transparency)
+			$colorxy = imagecolorat($src_im, $x, $y);
+			$alpha = ($colorxy >> 24) & 0xFF;
+
+			// Calculate new alpha
+			if ($minalpha !== 127)
+			{
+				$alpha = 127 + 127 * $pct * ($alpha - 127) / (127 - $minalpha);
+			}
+			else
+			{
+				$alpha += 127 * $pct;
+			}
+
+			// Get the color index with new alpha
+			$alphacolorxy = imagecolorallocatealpha($src_im, ($colorxy >> 16) & 0xFF, ($colorxy >> 8) & 0xFF, $colorxy & 0xFF, $alpha);
+
+			// Set pixel with the new color + opacity
+			if (!imagesetpixel($src_im, $x, $y, $alphacolorxy))
+			{
+				return false;
+			}
+		}
+	}
+
+	imagecopy($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h);
+}
 ?>
--- includes/ucp/ucp_dynamo.php
+++ includes/ucp/ucp_dynamo.php
@@ -23,60 +23,6 @@ if (!defined('IN_PHPBB'))
 */
 class ucp_dynamo
 {
-
-// From the php.net documentation: http://www.php.net/manual/en/function.imagecopymerge.php
-
-/**
- * PNG ALPHA CHANNEL SUPPORT for imagecopymerge();
- * This is a function like imagecopymerge but it handle alpha channel well!!!
- **/
-
-// A fix to get a function like imagecopymerge WITH ALPHA SUPPORT
-// Main script by aiden dot mail at freemail dot hu
-// Transformed to imagecopymerge_alpha() by rodrigo dot polo at gmail dot com 
-	function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){
-		if(!isset($pct)){
-		    return false;
-		}
-		$pct /= 100;
-		// Get image width and height
-		$w = imagesx( $src_im );
-		$h = imagesy( $src_im );
-		// Turn alpha blending off
-		imagealphablending( $src_im, false );
-		// Find the most opaque pixel in the image (the one with the smallest alpha value)
-		$minalpha = 127;
-		for( $x = 0; $x < $w; $x++ )
-		for( $y = 0; $y < $h; $y++ ){
-		    $alpha = ( imagecolorat( $src_im, $x, $y ) >> 24 ) & 0xFF;
-		    if( $alpha < $minalpha ){
-		        $minalpha = $alpha;
-		    }
-		}
-		//loop through image pixels and modify alpha for each
-		for( $x = 0; $x < $w; $x++ ){
-		    for( $y = 0; $y < $h; $y++ ){
-		        //get current alpha value (represents the TANSPARENCY!)
-		        $colorxy = imagecolorat( $src_im, $x, $y );
-		        $alpha = ( $colorxy >> 24 ) & 0xFF;
-		        //calculate new alpha
-		        if( $minalpha !== 127 ){
-		            $alpha = 127 + 127 * $pct * ( $alpha - 127 ) / ( 127 - $minalpha );
-		        } else {
-		            $alpha += 127 * $pct;
-		        }
-		        //get the color index with new alpha
-		        $alphacolorxy = imagecolorallocatealpha( $src_im, ( $colorxy >> 16 ) & 0xFF, ( $colorxy >> 8 ) & 0xFF, $colorxy & 0xFF, $alpha );
-		        //set pixel with the new color + opacity
-		        if( !imagesetpixel( $src_im, $x, $y, $alphacolorxy ) ){
-		            return false;
-		        }
-		    }
-		}
-		// The image copy
-		imagecopy($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h);
-	}
-
 	var $u_action;
 
 	function main($id, $mode)
@@ -182,7 +128,7 @@ class ucp_dynamo
 						// If an image is larger than the default then it will look weird
 						// Should find the largest width/height, and use that below
 						// Then resize it etc (so no cropping, preferrably)
-						$this->imagecopymerge_alpha($first_image, $this_image, 0, 0, 0, 0, $config['dynamo_width'], $config['dynamo_height'], 100);
+						merge_images($first_image, $this_image, 0, 0, 0, 0, $config['dynamo_width'], $config['dynamo_height'], 100);
 					}
 
 					imagesavealpha($first_image, true);
